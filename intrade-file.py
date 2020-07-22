e0b0fe1481b2ba2429e7bba08958645fe99fc473
TechTest95 <68619000+TechTest95@users.noreply.github.com>
2020-07-21 23:03:33 -0500

    Add files via upload

---
+++ intrade-file.py
@@ -0,0 +1,18 @@
+"""
+This is the intrade-data temp values:
+
+¡ THIS IS JUST AN EXPLANATION OF THE DATA !
+
+intrade-data = [
+    direction, -> specify the type of movement: IMPORTS / EXPORTS / REIMPORTS
+    year, 
+    date, 
+    country,
+    product,
+    transport mode, -> the type of transport used
+    measure, -> the measure of the movement: $ / Tonnes
+    value  -> the value of the measure
+    ]
+"""
+
+intrade-data = []
